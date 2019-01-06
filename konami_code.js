81713fb89fb8cd2ce1cd7d574fdd63749d7be22c
Christina Burchette <burchc426@gmail.com>
2019-01-06 16:44:41 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0; 
 
 function init(e) {
-  const key = parseInt(e.which);
+  let key = parseInt(e.which || );
   if (key===code[index]) {index++;
   
   if (index === code.length) {alert("Congrats!");
