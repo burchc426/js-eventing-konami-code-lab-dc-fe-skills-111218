d759dd1fc31921a0bfb066afae47c80562f7f275
Christina Burchette <burchc426@gmail.com>
2019-01-06 16:54:59 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,6 +2,11 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let index = 0; 
 
+function init() {
+  let body = document.body;
+  body.addEventListener('keydown', )
+}
+
 function init(e) {
   let body = document.body;
   
