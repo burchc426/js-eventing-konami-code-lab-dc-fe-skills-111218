9dd262f2b49b1f5aaf48161beabd45a86735b8b3
Christina Burchette <burchc426@gmail.com>
2019-01-06 16:59:08 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -21,21 +21,3 @@ function init() {
   }
 }
 
-function init(e) {
-  let body = document.body;
-  
-  body.addEventListener('keydown', konami(e) {
-    
-    let key = parseInt(e.which || e.detail);
-    
-  if (key===code[index]) {index++;
-  
-  if (index === code.length-1) { index = 0;
-  alert("Congrats!");
- }
-  
-  } else {
-    index = 0;
-  }
-
-)}}
