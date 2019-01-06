14db3c78dbfeee7e3dc1c136d9ab9eafaf00258a
Christina Burchette <burchc426@gmail.com>
2019-01-06 16:50:01 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -4,9 +4,10 @@ let index = 0;
 
 function init(e) {
   let body = document.body;
+  
   body.addEventListener('keydown', konami(e)) {
- 
- let key = parseInt(e.which || e.detail);
+    
+    let key = parseInt(e.which || e.detail);
   if (key===code[index]) {index++;
   
   if (index === code.length) {alert("Congrats!");
